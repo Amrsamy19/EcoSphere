2335f052cafd666dd7a5bd3c5a1af892fbad0232
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-18 00:36:17 +0200

    feat: add the product card with dummy data

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,12 +1,14 @@
 import StoreSlider from "@/components/layout/store/StoreSlider";
 import StoreHero from "@/components/layout/store/StoreHero";
 import React from "react";
+import ProductCardSection from "@/components/layout/store/ProductCardSection";
 
 export default function Store() {
   return (
     <div className="space-y-5 md:space-y-10">
       <StoreHero />
       <StoreSlider />
+      <ProductCardSection />
     </div>
   );
 }
