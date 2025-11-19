fe435f2ea3d7bd88a0b86b3cb19aa5a67d87589d
marwan <marwanabdalmagied@gmail.com>
2025-11-20 01:26:50 +0200

    docs: add drawio file to show the db schema

--- src/backend/features/auth/auth.controller.ts
+++ src/backend/features/auth/auth.controller.ts
@@ -5,38 +5,38 @@ import "reflect-metadata";
 
 @injectable()
 class AuthController {
-  constructor(
-    @inject("IAuthService") private readonly IAuthService: IAuthService
-  ) {}
+	constructor(
+		@inject("IAuthService") private readonly IAuthService: IAuthService
+	) {}
 
-  async LogIn(
-    email: string,
-    password: string
-  ): Promise<{ token: string; user: Omit<User, "password"> } | null> {
-    return await this.IAuthService.login(email, password);
-  }
+	async LogIn(
+		email: string,
+		password: string
+	): Promise<{ token: string; user: Omit<User, "password"> } | null> {
+		return await this.IAuthService.login(email, password);
+	}
 
-  async Register(body: {
-    email: string;
-    name: string;
-    password: string;
-    birthDate: string;
-    address: string;
-    avatar: string;
-    gender: string;
-    phoneNumber : string,
-  }): Promise<{ token: string; user: Omit<User, "password"> } | null> {
-    return await this.IAuthService.register(
-      body.email,
-      body.name,
-      body.password,
-      body.birthDate,
-      body.address,
-      body.avatar,
-      body.gender,
-      body.phoneNumber,
-    );
-  }
+	async Register(body: {
+		email: string;
+		name: string;
+		password: string;
+		birthDate: string;
+		address: string;
+		avatar: string;
+		gender: string;
+		phoneNumber: string;
+	}): Promise<{ token: string; user: Omit<User, "password"> } | null> {
+		return await this.IAuthService.register(
+			body.email,
+			body.name,
+			body.password,
+			body.birthDate,
+			body.address,
+			body.avatar,
+			body.gender,
+			body.phoneNumber
+		);
+	}
 }
 
 export default AuthController;
