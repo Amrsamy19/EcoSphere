32d368c24db45deda47208bba5c73b6cb82e7691
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:11:40 +0200

    feat: add register/login request/response dto

--- src/backend/features/auth/dto/user.dto.ts
+++ src/backend/features/auth/dto/user.dto.ts
@@ -1,16 +1,30 @@
-export type LoginDto = {
-  email: string;
-  password: string;
+import { TokenPayload } from "@/backend/interfaces/interfaces";
+import { UserRole } from "../../user/user.model";
+
+export type LoginRequestDTO = {
+	email: string;
+	password: string;
+	userType: UserRole;
 };
 
-export type RegisterDto = {
-  email: string;
-  name: string;
-  password: string;
-  birthDate: string;
-  address: string;
-  avatar: string;
-  gender: string;
-  phoneNumber: string;
+export type LoginResponseDTO = {
+	token: string;
+	user: TokenPayload;
 };
 
+export type RegisterRequestDTO = {
+	email: string;
+	name: string;
+	password: string;
+	birthDate: string;
+	address: string;
+	avatar: string;
+	gender: string;
+	phoneNumber: string;
+	role: UserRole;
+};
+
+export type RegisterResponseDTO = {
+	token: string;
+	user: TokenPayload;
+};
