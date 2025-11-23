e94758dcfda7a68b16f2bf8347b6fc002a35837e
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:20:23 +0200

    fix: fix broken user login after strategy pattern

--- src/backend/features/auth/login/user.login.ts
+++ src/backend/features/auth/login/user.login.ts
@@ -1,12 +1,33 @@
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ILoginStrategy } from "./login.service";
-
+import { type IAuthRepository } from "../auth.repository";
+import type { LoginRequestDTO, LoginResponseDTO } from "../dto/user.dto";
+import { generateToken, OMIT } from "@/backend/utils/helpers";
 @injectable()
 class UserLoginStrategy implements ILoginStrategy {
-	constructor() {}
-	login<T, V>(date: T): Promise<V> {
-		console.log(date);
-		throw new Error("Method not implemented, user login");
+	constructor(
+		@inject("IAuthRepository") private readonly authRepository: IAuthRepository
+	) {}
+	async login(data: LoginRequestDTO): Promise<LoginResponseDTO> {
+		const user = await this.authRepository.findByEmail(data.email);
+
+		if (!user) throw new Error("User not found");
+		if (!(await user.comparePassword(data.password)))
+			throw new Error("Invalid email or password");
+
+		const token = generateToken({
+			id: user._id!,
+			lastName: user.lastName,
+			email: user.email,
+			role: user.role,
+		});
+
+		const response = OMIT(user, "password");
+		return {
+			token,
+			user: response._doc,
+		};
 	}
 }
+
 export { UserLoginStrategy };
