adaf950d670dd0d1c7a6057004f57b89a75f922f
marwan <marwanabdalmagied@gmail.com>
2025-11-23 14:07:27 +0200

    refactor: rename a typo and some services in the container

--- src/app/api/resturants/[id]/route.ts
+++ src/app/api/resturants/[id]/route.ts
@@ -1,25 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import { rootContainer } from "@/backend/config/container";
-import RestaurantController from "@/backend/features/resturant/resturant.controller";
+import RestaurantController from "@/backend/features/restaurant/restaurant.controller";
 
-export const GET = async (req: NextRequest, context: { params: Promise<{ id: string }> }) => {
-    const { id } = await context.params;
-    const controller = rootContainer.resolve(RestaurantController);
-    const result = await controller.getById(id);
-    return NextResponse.json({ message: "hello after db connection", result });
-}
+export const GET = async (
+	req: NextRequest,
+	context: { params: Promise<{ id: string }> }
+) => {
+	const { id } = await context.params;
+	const controller = rootContainer.resolve(RestaurantController);
+	const result = await controller.getById(id);
+	return NextResponse.json({ message: "hello after db connection", result });
+};
 
-export const PUT = async (req: NextRequest, context: { params: Promise<{ id: string }> }) => {
-    const { id } = await context.params;
-    const body = await req.json();
-    const controller = rootContainer.resolve(RestaurantController);
-    const result = await controller.updateById(id, body);
-    return NextResponse.json({ message: "hello after db connection", result });
-}
+export const PUT = async (
+	req: NextRequest,
+	context: { params: Promise<{ id: string }> }
+) => {
+	const { id } = await context.params;
+	const body = await req.json();
+	const controller = rootContainer.resolve(RestaurantController);
+	const result = await controller.updateById(id, body);
+	return NextResponse.json({ message: "hello after db connection", result });
+};
 
-export const DELETE = async (req: NextRequest, context: { params: Promise<{ id: string }> }) => {
-    const { id } = await context.params;
-    const controller = rootContainer.resolve(RestaurantController);
-    const result = await controller.deleteById(id);
-    return NextResponse.json({ message: "hello after db connection", result });
-}
+export const DELETE = async (
+	req: NextRequest,
+	context: { params: Promise<{ id: string }> }
+) => {
+	const { id } = await context.params;
+	const controller = rootContainer.resolve(RestaurantController);
+	const result = await controller.deleteById(id);
+	return NextResponse.json({ message: "hello after db connection", result });
+};
--- src/app/api/resturants/route.ts
+++ src/app/api/resturants/route.ts
@@ -1,17 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { rootContainer } from "@/backend/config/container";
-import RestaurantController from "@/backend/features/resturant/resturant.controller";
+import RestaurantController from "@/backend/features/restaurant/restaurant.controller";
 
 export const GET = async (req: NextRequest) => {
-    const controller = rootContainer.resolve(RestaurantController);
-    const result = await controller.getAll();
-    return NextResponse.json({ message: "hello after db connection", result });
-}
+	const controller = rootContainer.resolve(RestaurantController);
+	const result = await controller.getAll();
+	return NextResponse.json({ message: "hello after db connection", result });
+};
 
 export const POST = async (req: NextRequest) => {
-    const body = await req.json();
-    const { location, rating, name, workingHours, phoneNumber, avatar, description } = body;
-    const controller = rootContainer.resolve(RestaurantController);
-    const result = await controller.create(location, rating, name, workingHours, phoneNumber, avatar, description);
-    return NextResponse.json({ message: "hello after db connection", result });
-}
+	const body = await req.json();
+	const {
+		location,
+		rating,
+		name,
+		workingHours,
+		phoneNumber,
+		avatar,
+		description,
+	} = body;
+	const controller = rootContainer.resolve(RestaurantController);
+	const result = await controller.create(
+		location,
+		rating,
+		name,
+		workingHours,
+		phoneNumber,
+		avatar,
+		description
+	);
+	return NextResponse.json({ message: "hello after db connection", result });
+};
