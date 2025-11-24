35e217f5ed51c8a07965fc934c4aaf1378cee72d
hebaehab2001 <hebaehab74@gmail.com>
2025-11-24 09:55:07 +0200

    handle theme error

--- src/components/layout/SideBar/ThemeBtn/ThemeBtn.tsx
+++ src/components/layout/SideBar/ThemeBtn/ThemeBtn.tsx
@@ -15,6 +15,8 @@ import {
 
 export default function ThemeBtn() {
     const { theme, setTheme } = useTheme();
+    console.log(theme);
+    
     return (
         <SidebarMenuItem >
             <DropdownMenu>
@@ -23,7 +25,8 @@ export default function ThemeBtn() {
                         <span >
                             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
                             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                            <span >{theme} theme</span>
+                            {/* <span >{theme === 'dark' ? 'Dark' : theme==='light'?'Light':'System'} theme</span> */}
+                            <span className='capitalize' >{`${theme}`} theme</span>
                         </span>
                     </SidebarMenuButton>
                 </DropdownMenuTrigger>
