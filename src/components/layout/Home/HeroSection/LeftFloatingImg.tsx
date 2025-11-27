1a391d3ccdae6341458dcf16ab67172045ccc9a9
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-27 19:14:17 +0200

    feat: add cursor pointer to the imgs

--- src/components/layout/Home/HeroSection/LeftFloatingImg.tsx
+++ src/components/layout/Home/HeroSection/LeftFloatingImg.tsx
@@ -10,7 +10,7 @@ const LeftFloatingImg = () => {
           height={500}
           alt="left floating img"
           src="/homeHero/event.png"
-          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer"
         />
       </div>
       <div className="absolute left-[20%] bottom-[20%] lg:bottom-auto lg:top-[45%] lg:left-[5%] w-[35%] rounded-full aspect-square z-20 ">
@@ -19,7 +19,7 @@ const LeftFloatingImg = () => {
           height={500}
           alt="left floating img"
           src="/homeHero/game.png"
-          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer"
         />
       </div>
       <div className="absolute left-[60%] bottom-[1%] lg:bottom-auto lg:top-[60%] lg:left-[55%] w-[35%] rounded-full aspect-square z-20">
@@ -28,7 +28,7 @@ const LeftFloatingImg = () => {
           height={500}
           alt="left floating img"
           src="/homeHero/recycle.png"
-          className="w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className="w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out cursor-pointer"
         />
       </div>
     </div>
