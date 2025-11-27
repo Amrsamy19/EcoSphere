c7374d2ee4a105fb0497f71fb2370e21fb2608c9
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-27 19:09:55 +0200

    feat: add all positions to the lefd floating imgs

--- src/components/layout/Home/HeroSection/LeftFloatingImg.tsx
+++ src/components/layout/Home/HeroSection/LeftFloatingImg.tsx
@@ -4,31 +4,31 @@ import React from "react";
 const LeftFloatingImg = () => {
   return (
     <div className=" relative h-screen ">
-      <div className="absolute lg:top-[20%] lg:left-[40%] w-[35%] rounded-full aspect-square z-20 ">
+      <div className="absolute left-[7%] bottom-[50%] lg:bottom-auto lg:top-[20%] lg:left-[40%] w-[35%] rounded-full aspect-square z-20 ">
         <Image
           width={500}
           height={500}
           alt="left floating img"
           src="/homeHero/event.png"
-          className=" w-full h-full rounded-full aspect-square border-2 border-primary overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
         />
       </div>
-      <div className="absolute lg:top-[45%] lg:left-[5%] w-[35%] rounded-full aspect-square z-20 ">
+      <div className="absolute left-[20%] bottom-[20%] lg:bottom-auto lg:top-[45%] lg:left-[5%] w-[35%] rounded-full aspect-square z-20 ">
         <Image
           width={500}
           height={500}
           alt="left floating img"
           src="/homeHero/game.png"
-          className=" w-full h-full rounded-full aspect-square border-2 border-primary overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className=" w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
         />
       </div>
-      <div className="absolute lg:top-[60%] lg:left-[55%] w-[35%] rounded-full aspect-square z-20">
+      <div className="absolute left-[60%] bottom-[1%] lg:bottom-auto lg:top-[60%] lg:left-[55%] w-[35%] rounded-full aspect-square z-20">
         <Image
           width={500}
           height={500}
           alt="left floating img"
           src="/homeHero/recycle.png"
-          className="w-full h-full rounded-full aspect-square border-2 border-primary overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
+          className="w-full h-full rounded-full aspect-square border-2 border-background overflow-hidden hover:scale-105 transition-all duration-500 ease-in-out"
         />
       </div>
     </div>
