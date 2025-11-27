306a477da7f2213fd04bf2695c270ae2615eed60
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-27 18:11:39 +0200

    feat: make the structure of floating img containers

--- src/components/layout/Home/HeroSection/HomeHeroLayout.tsx
+++ src/components/layout/Home/HeroSection/HomeHeroLayout.tsx
@@ -3,8 +3,13 @@ import HomeHero from "../HomeHero";
 
 const HomeHeroLayout = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen relative">
       <HomeHero />
+      <div className="absolute inset-0 gap-5 h-screen w-full hidden md:grid md:grid-cols-2 lg:grid-cols-3">
+        <div className="bg-amber-600"></div>
+        <div className="hidden lg:block"></div>
+        <div className="bg-amber-600"></div>
+      </div>
     </div>
   );
 };
