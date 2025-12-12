8a37be6de237af108f5973e1f165d443fded6978
marwan <marwanabdalmagied@gmail.com>
2025-12-12 23:27:32 +0200

    refactor: add rest id with and create one new type

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,5 +1,6 @@
 export interface IProduct {
   id: string;
+  restaurantId: string;
   shopName: string;
   shopSubtitle: string;
   productImg: string;
@@ -7,4 +8,8 @@ export interface IProduct {
   productPrice: number;
   productSubtitle: string;
   productDescription: string;
+}
+
+export interface IProductCart extends IProduct {
+  quantity: number;
 }
